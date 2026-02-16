794e5cdf65c8c5f16cdb6667df807b9f414c7ad5
Filipe Dávila <filipedavila6@gmail.com>
2026-02-16 17:18:04 +0000

    Export entry point for npm package

---
+++ metadata-package/src/index.ts
@@ -0,0 +1,8 @@
+export { ruleset } from './tests-metadata';
+export { testColors } from './tests_colors';
+export { techniques } from './tech';
+import * as pt from '../locale/Portuguese.json';
+import * as en from '../locale/English.json';
+
+
+export const translations = { pt, en } as const;
